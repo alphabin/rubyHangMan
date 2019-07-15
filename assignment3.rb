64151bc43ce098a6526805cab6e1f80c88b3a5d7
alphabin <ec2-user@ip-172-31-32-181.us-east-2.compute.internal>
2019-07-15 06:15:51 +0000

    hangman Game core code complete

--- assignment3.rb
+++ assignment3.rb
@@ -1,12 +1,14 @@
 class Hangman
 
-  attr_reader :word, :count
+  attr_reader :word, :count, :guessedTrue,:guessedFalse
 
   def initialize(word, count)
     @word = word
+    @shadowWord =  Array.new(@word.length, "_")
     @count = count
     @win = false
-
+    @guessedTrue = []
+    @guessedFalse = []
   end 
   
   def gameOver?
@@ -22,13 +24,35 @@ class Hangman
   # otherwise, return the number of times the letter 
   # appears in the word
   def guess(letter)
- 
+      #find the guess
+      returnValue = @word.count(letter) 
+      case returnValue
+        when 0
+          #Meaning this was an invalid guess
+          @count-=1
+          @guessedFalse << letter
+        else
+          @guessedTrue << letter
+          arryOfMatchesIndexs = (0 ... @word.length).find_all { |i| @word[i,1] == letter }
+          arryOfMatchesIndexs.each { |x| 
+            @shadowWord[x] = letter 
+          }
+          
+          if (@shadowWord.index("_")).nil? 
+            @win = true
+          end
+          
+          
+      end
+        returnValue
   end 
   
   # return one of the letters in the word 
   # that has not been guessed 
   def hint()
-  
+      hintIndex =  @shadowWord.index("_");
+      @count-=1
+      @word[hintIndex]
   end 
   
   # return a string 
@@ -38,7 +62,11 @@ class Hangman
   # Example:  if the word is cookie, and o and e have been guessed
   #   return the string "_ o o _ _ e"
   def display()
-  
+      presentWord = ""
+      @shadowWord.each { |x| 
+        presentWord += x + " "
+      }
+      presentWord.rstrip
   end
  
 end
